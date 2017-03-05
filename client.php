02c03b83489381d9e3652bceb8a86d9d9cc6e980
alonsabi <alon@functionpoint.com>
2017-03-04 22:27:21 -0800

    New client that supports multiple users, and global enable disable, as well as all the basic productivity apps to run in all cases

--- client.php
+++ client.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
-
 <?php
+date_default_timezone_set('America/Vancouver');
 require_once(__DIR__.'/config.php');
 
 define('SERVERLOCATION', $server);
@@ -10,21 +10,27 @@ define('USERNAME', $user);
 $appsOnServer = serverRequest('q=getAll');
 if ($appsOnServer) {
     $appsOnServer = unserialize(base64_decode($appsOnServer));
-    $allDisabled = true;
-    foreach ($appsOnServer as $appInfo) {
-        if ($appInfo['enabled']) {
-            $allDisabled = false;
-        }
-    }
 
-    // If everything is disabled, we do not want to restrict
-    $appsOnServer['Finder'] = array('appName' => 'Finder', 'enabled' => 1); // make sure finder is always allowed
-    $appsOnServer['Terminal'] = array('appName' => 'Terminal', 'enabled' => 1); // make sure finder is always allowed
+    // If restrictions are on, we still want to be able to run some basic apps, for me to be able to use it.
+    $appsOnServer['Finder'] = array('appName' => 'Finder', 'enabled' => 1);
+    $appsOnServer['Terminal'] = array('appName' => 'Terminal', 'enabled' => 1);
+    $appsOnServer['iTerm2'] = array('appName' => 'iTerm2', 'enabled' => 1);
+    $appsOnServer['phpstorm'] = array('appName' => 'phpstorm', 'enabled' => 1);
+
 } else {
     // There is no connection to the server for some reason, we allow only specific things to run
-    $appsOnServer['Finder'] = array('appName' => 'Finder', 'enabled' => 1); // make sure finder is always allowed
-    $appsOnServer['Terminal'] = array('appName' => 'Terminal', 'enabled' => 1); // make sure finder is always allowed
-    $appsOnServer['Safari'] = array('appName' => 'Safari', 'enabled' => 1); // make sure finder is always allowed
+    $appsOnServer['Finder'] = array('appName' => 'Finder', 'enabled' => 1);
+    $appsOnServer['Terminal'] = array('appName' => 'Terminal', 'enabled' => 1);
+    $appsOnServer['Safari'] = array('appName' => 'Safari', 'enabled' => 1);
+    $appsOnServer['phpstorm'] = array('appName' => 'phpstorm', 'enabled' => 1);
+    $appsOnServer['Microsoft Excel'] = array('appName' => 'Microsoft Excel', 'enabled' => 1);
+    $appsOnServer['Microsoft PowerPoint'] = array('appName' => 'Microsoft PowerPoint', 'enabled' => 1);
+    $appsOnServer['Microsoft Word'] = array('appName' => 'Microsoft Word', 'enabled' => 1);
+    $appsOnServer['Numbers'] = array('appName' => 'Numbers', 'enabled' => 1);
+    $appsOnServer['Pages'] = array('appName' => 'Pages', 'enabled' => 1);
+    $appsOnServer['Preview'] = array('appName' => 'Preview', 'enabled' => 1);
+    $appsOnServer['Keynote'] = array('appName' => 'Keynote', 'enabled' => 1);
+    $appsOnServer['Mail'] = array('appName' => 'Mail', 'enabled' => 1);
 }
 
 ob_start();
@@ -39,7 +45,10 @@ foreach ($runningApps as $appName) {
         }
         error_log("\n".$appName.' '.date('Y-m-d H:i:s'),3,'/var/tmp/restrict_log.txt');
         //echo "killing $appName\n";
-        passthru('/usr/bin/killall "'.$appName.'"');
+        if ($appsOnServer['Enable Restrictions']['enabled'] == 1) {
+            echo "closing $appName\n";
+            passthru('/usr/bin/killall "' . $appName . '"');
+        }
     }
 }
 
